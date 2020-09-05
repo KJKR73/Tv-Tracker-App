06c788efd43eee57646b0102612457db585841f2
Karanjot Singh <kjkr7373@gmail.com>
2020-09-05 21:04:13 +0530

    Memory leak fixed

--- lib/screens/watching/watching_ind.dart
+++ lib/screens/watching/watching_ind.dart
@@ -1,8 +1,8 @@
 import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:http/http.dart';
+import 'package:flutter/services.dart';
 
 class WatchInd extends StatefulWidget {
   @override
@@ -23,48 +23,41 @@ class _WatchIndState extends State<WatchInd> {
     if (total == '?') {
       return false;
     }
-    if (total == current.toString()) {
+    if (int.parse(current) < 0) {
+      return true;
+    }
+    if (int.parse(total) < int.parse(current)) {
       return true;
     } else {
       return false;
     }
   }
 
-  bool verfiyDec(current) {
-    if (current == 0) {
-      return true;
-    }
-    return false;
-  }
-
+  final _formKey = GlobalKey<FormState>();
   int current;
   String total;
   Map data;
   var body;
-
   @override
   Widget build(BuildContext context) {
     data = data == null ? ModalRoute.of(context).settings.arguments : data;
     body = data['data'];
-    current = body['current'];
     total = body['total'];
-    print(body);
-    print(ModalRoute.of(context).settings.arguments);
     return Scaffold(
       backgroundColor: Colors.black,
       body: SafeArea(
         child: Container(
-          color: Colors.grey[800],
+          color: Colors.grey[850],
           child: Column(
             children: [
               Expanded(
-                flex: 9,
+                flex: 7,
                 child: FutureBuilder(
                   future: _getImage(data["body"]),
                   builder: (context, snapshot) {
                     if (snapshot.hasData) {
                       return Container(
-                        width: MediaQuery.of(context).size.width,
+                        width: MediaQuery.of(context).size.width - 80,
                         child: Image.memory(
                           snapshot.data,
                           fit: BoxFit.fill,
@@ -85,43 +78,91 @@ class _WatchIndState extends State<WatchInd> {
               Expanded(
                 flex: 4,
                 child: Container(
+                  padding: EdgeInsets.symmetric(horizontal: 10),
                   child: Form(
                     child: Column(
                       children: [
-                        Row(
-                          children: [
-                            Text(
-                              "Current: ",
-                            ),
-                            Text(
-                              data['data']['current'].toString() + '/',
-                            ),
-                            Text(
-                              data['data']['total'],
-                            ),
-                            FlatButton(
-                              color: Colors.blue,
-                              child: Text("+"),
-                              onPressed: verfiyInc(current, total)
-                                  ? null
-                                  : () {
+                        Text(
+                          "Update Info",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 26,
+                          ),
+                        ),
+                        ListTile(
+                          title: Row(
+                            children: [
+                              Expanded(
+                                flex: 3,
+                                child: Text(
+                                  "Current: ",
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontFamily: "GM",
+                                    fontSize: 26,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ),
+                              Expanded(
+                                flex: 1,
+                                child: Form(
+                                  key: _formKey,
+                                  child: TextFormField(
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 20,
+                                    ),
+                                    initialValue: body['current'].toString(),
+                                    onChanged: (val) {
                                       setState(() {
-                                        body['current'] += 1;
+                                        this.current = int.parse(val);
+                                        imageCache.clear();
                                       });
                                     },
-                            ),
-                            FlatButton(
-                              color: Colors.blue,
-                              child: Text("-"),
-                              onPressed: verfiyDec(current)
-                                  ? null
-                                  : () {
-                                      setState(() {
-                                        body['current'] -= 1;
-                                      });
+                                    validator: (value) {
+                                      if (!verfiyInc(value, total)) {
+                                        return null;
+                                      }
+                                      return "NV";
                                     },
-                            ),
-                          ],
+                                    keyboardType: TextInputType.number,
+                                  ),
+                                ),
+                              ),
+                              Expanded(
+                                flex: 1,
+                                child: Text(
+                                  '/',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontFamily: "GM",
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ),
+                              Expanded(
+                                flex: 1,
+                                child: Text(
+                                  total,
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontFamily: "GM",
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ),
+                              RaisedButton(
+                                onPressed: () {
+                                  if (_formKey.currentState.validate()) {
+                                    print(current);
+                                  }
+                                },
+                              )
+                            ],
+                          ),
                         )
                       ],
                     ),
@@ -135,3 +176,56 @@ class _WatchIndState extends State<WatchInd> {
     );
   }
 }
+
+// Expanded(
+//   flex: 1,
+//   child: FlatButton(
+//     color: Colors.blue,
+//     child: Icon(Icons.add),
+//     onPressed: verfiyInc(current, total)
+//         ? null
+//         : () {
+//             setState(() {
+//               body['current'] += 1;
+//             });
+//           },
+//   ),
+// ),
+// Expanded(
+//   flex: 1,
+//   child: Text(
+//     data['data']['current'].toString() + '/',
+//     style: TextStyle(
+//       color: Colors.white,
+//       fontFamily: "GM",
+//       fontSize: 16,
+//       fontWeight: FontWeight.bold,
+//     ),
+//   ),
+// ),
+// Expanded(
+//   flex: 1,
+//   child: Text(
+//     data['data']['total'],
+//     style: TextStyle(
+//       color: Colors.white,
+//       fontFamily: "GM",
+//       fontSize: 16,
+//       fontWeight: FontWeight.bold,
+//     ),
+//   ),
+// ),
+// Expanded(
+//   flex: 1,
+//   child: FlatButton(
+//     color: Colors.blue,
+//     child: Icon(Icons.subway),
+//     onPressed: verfiyDec(current)
+//         ? null
+//         : () {
+//             setState(() {
+//               body['current'] -= 1;
+//             });
+//           },
+//   ),
+// ),
